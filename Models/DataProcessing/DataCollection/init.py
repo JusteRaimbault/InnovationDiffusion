cc546e5adce59852b97847857ca3f624e0fcb650
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-12-13 22:18:53 +0100

    .

--- Models/DataProcessing/DataCollection/init.py
+++ Models/DataProcessing/DataCollection/init.py
@@ -2,8 +2,17 @@ import pymongo,utils
 
 DATABASE='uspto'
 #DATABASE='test'
-MINID=150
-MAXID=4200000
+
+# first phase
+#MINID=150
+#MAXID=4200000
+
+# on 2018/12/13, latest id is 10149420
+MINID=4200001
+MAXID=10149420
+drop=False
+
+# testing
 #MINID=9000
 #MAXID=10000
 
@@ -11,7 +20,8 @@ MAXID=4200000
 mongo = pymongo.MongoClient(utils.get_parameter('mongopath',True,True))
 database = mongo[DATABASE]
 
-database['tocollect'].drop()
+#if drop :
+#    database['tocollect'].drop()
 
 # fill id coll with id to collect
 ids = [{'id':i,'attempts':0} for i in list(range(MINID,MAXID))]

672218794e8cf81f1b735de706f27b4c635e18ab
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-10-08 05:50:45 +0100

    correct_bin_texts

--- Models/DataCollection/Collection/parse.py
+++ Models/DataCollection/Collection/parse.py
@@ -22,6 +22,9 @@ for i in range(0,BULKSIZE):
             if parser.validate(parsed):
                 successnum = successnum + 1
                 #print(parser.to_string(parsed))
+                # !!! FAIL binary data for text
+                #! TODO
+
                 database['patents'].insert_one(parsed)
             else :
                 # put again id in tocollect
